5ed1fff8bf361ab90f97a68b5f1b3bcf342a0051
Tommaso Anselmi <tommaso@tommasoanselmi.it>
2026-01-25 14:53:13 +0100

    Fixed margin for link in content block

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -33,7 +33,7 @@ export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
               >
                 {richText && <RichText data={richText} enableGutter={false} />}
 
-                {enableLink && <CMSLink {...link} />}
+                {enableLink && <CMSLink {...link} className="mt-3" />}
               </div>
             )
           })}
